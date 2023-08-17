0cb612f968bb0faedc4e144b68c44cf5cec6e5e4
lza <307321587@qq.com>
2023-08-17 19:31:08 +0800

    最简单qimage传输

--- cpp_pubsub/cpp_pubsub_board/build.sh
+++ cpp_pubsub/cpp_pubsub_board/build.sh
@@ -5,6 +5,13 @@ export TARGET_ARCH=aarch64
 export TARGET_TRIPLE=aarch64-linux-gnu
 export CROSS_COMPILE=/opt/gcc-ubuntu-9.3.0-2020.03-x86_64-aarch64-linux-gnu/bin/$TARGET_TRIPLE-
 
+colcon build --packages-select qt_image --merge-install --cmake-force-configure --cmake-args \
+--no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE=\
+`pwd`/aarch64_toolchainfile.cmake
+
+source install/setup.bash
+
 colcon build --packages-select cpp_pubsub --merge-install --cmake-force-configure --cmake-args \
 --no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE=\
 `pwd`/aarch64_toolchainfile.cmake
+
